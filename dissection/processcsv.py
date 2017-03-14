14d08964beb7de06fa26e21b881e1bc59ae19b57
David Bau <david.bau@gmail.com>
2017-03-14 07:32:23 -0400

    Set up experiment.

---
+++ dissection/processcsv.py
@@ -0,0 +1,16 @@
+import csv, sys, json
+
+imgpat = ('http://people.csail.mit.edu/davidbau/dissection' +
+  '/reference_places205/image/conv5-%04d.jpg')
+
+all_labels = False
+with open(sys.argv[1]) as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        result = {
+            'unit': int(row['unit']),
+            'image': imgpat % int(row['unit']),
+            'phrase': row['label']
+        }
+        print json.dumps(result)
+
