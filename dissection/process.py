5e70610ba9c941449ec299507cce7cc0c2670c53
David Bau <david.bau@gmail.com>
2017-03-15 16:58:46 -0400

    Update template

--- dissection/process.py
+++ dissection/process.py
@@ -1,27 +1,69 @@
 import csv, sys, json, random
+from scipy.io import loadmat
+import scrape
+
 
 imgpat = ('http://people.csail.mit.edu/davidbau/dissection' +
   '/reference_places205/image/conv5-5-%04d.jpg')
 
+iccv_file = '/home/davidbau/interp/iccv/caffe_reference_places205/conv5-result.csv'
 rng = random.Random(1)
 
 all_labels = False
 batch_size = 20
-with open(sys.argv[1]) as f:
+
+with open(iccv_file) as f:
     reader = csv.DictReader(f)
     cases = []
     for row in reader:
         # TODO: consider evaluating cases
         cases.append({
+            'method': 'iccv',
             'unit': int(row['unit']),
             'image': imgpat % (int(row['unit']) - 1),
             'phrase': row['label']
         })
 
-rng.shuffle(cases)
-# Rount up the number of batches so that the size of batches is no
-# more than batch_size.  Batches will be evenly distributed.
-batch_count = (len(cases) + batch_size - 1) // batch_size
-for index in range(batch_count):
-    batch = cases[index::batch_count]
-    print json.dumps({ 'cases': batch })
+# TODO: add CVPR data from scrape.py
+cvpr = scrape.cvpr_data()
+for unit, row in enumerate(cvpr):
+    cases.append({
+        'method': 'cvpr',
+        'unit': (unit + 1),
+        'image': imgpat % unit,
+        'phrase': row['score'][0][1].lower()
+        })
+
+# TODO: add human data from import.py
+human = loadmat('iclr/neuronAnnotation_places205_afterICLR.mat',
+        squeeze_me=True)['layerAnnotation'][4,0]
+for unit, labels in enumerate(human):
+    alternatives = []
+    for j in range(0, len(labels), 3):
+        alternatives.append((-labels[j + 2], labels[j]))
+    alternatives.sort()
+    cases.append({
+        'method': 'human',
+        'unit': (unit + 1),
+        'image': imgpat % unit,
+        'phrase': alternatives[0][1]
+    })
+
+# Complicated shuffling to avoid duplicate unit in the same HIT batch
+units = [[] for _ in range(256)]
+for c in cases:
+    units[c['unit'] - 1].append(c)
+
+for u in units:
+    rng.shuffle(u)
+
+for b in range(0, 3):
+    cases = [u[b] for u in units]
+    rng.shuffle(cases)
+    batch_count = (len(cases) + batch_size - 1) // batch_size
+    batches = []
+    for index in range(batch_count):
+        batch = cases[index::batch_count]
+        assert len(set([b['unit'] for b in batch])) == len(batch)
+        print json.dumps({ 'cases': batch })
+
---
+++ dissection/scrape.py
@@ -0,0 +1,51 @@
+import urllib2, urllib, urlparse
+import re, math, os
+
+'''
+Example:
+
+<h2>256. unit 208 confidence:0.058 (object)</h2>
+<p>predicted labels:</p><strong>    object</strong>: tree (0.058) windowpane (0.038) house (0.036) mountain (0.034) grass (0.029)<br>
+<strong>      part</strong>: pane (0.046) wheel (0.024) hair (0.010) windowpane (0.009) chimney (0.008)<br>
+<strong>   texture</strong>: crosshatched (-0.017) porous (-0.018) potholed (-0.019) crystalline (-0.019) braided (-0.019)<br>
+<strong>  material</strong>: glass (-0.017) wallpaper (-0.020) granite (-0.024) concrete (-0.027) food (-0.028)<br>
+<strong>     color</strong>: white-c (-0.077) grey-c (-0.078) blue-c (-0.080) brown-c (-0.081) yellow-c (-0.083)<br>
+<p><img src="unit_segmentation_singlelayer/places205_baseline/unitID208.jpg" /></p>
+'''
+
+def read_listing_page(url):
+    '''
+    returns an array of dicts.
+    Each dict has a u
+    '''
+    webpage = urllib2.urlopen(url)
+    records = []
+    record = None
+    for line in webpage.readlines():
+        m = re.search(r'<h2>(?:\d+\. )?unit (\d+).*', line)
+        if m:
+            record = {'unit': int(m.group(1)), 'score': []}
+        m = re.search(r'''(?x)
+            <strong>\s*(object|part|texture|material|color)</strong>:
+            \s*([^\(\)]+)\s\((-?[\d\.]+)\)
+            ''', line)
+        if m:
+            record['score'].append((m.group(1), m.group(2),float(m.group(3))))
+        m = re.search(r'''(?x)
+            <img\s+src="([^"]*)"
+            ''', line)
+        if m and not 'validation set' in line:
+            record['img'] = urllib.unquote(m.group(1))
+            records.append(record)
+            record = None
+    return records
+
+
+# Old url - was overwritten by a different approach
+url = 'http://places.csail.mit.edu/deepscene/hybercolumn/plot_cvpr/precision_unit2label_places205_baseline.html'
+
+# New url - same data
+url = 'http://places.csail.mit.edu/deepscene/hybercolumn/plot_cvpr/precision_unit2label_places1_caffenet_unisegV4/conv5.html'
+
+def cvpr_data():
+    return read_listing_page(url)
