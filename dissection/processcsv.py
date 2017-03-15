6d08c1d8ca940e388a69d6d7a8bf3bd25a637803
David Bau <david.bau@gmail.com>
2017-03-15 00:41:13 -0400

    Draft of new dissection eval ui.

--- dissection/processcsv.py
+++ dissection/processcsv.py
@@ -1,16 +1,24 @@
-import csv, sys, json
+import csv, sys, json, random
 
 imgpat = ('http://people.csail.mit.edu/davidbau/dissection' +
-  '/reference_places205/image/conv5-%04d.jpg')
+  '/reference_places205/image/conv5-5-%04d.jpg')
+
+rng = random.Random(1)
 
 all_labels = False
+batch_size = 64
 with open(sys.argv[1]) as f:
     reader = csv.DictReader(f)
+    cases = []
     for row in reader:
-        result = {
+        # TODO: consider evaluating cases
+        cases.append({
             'unit': int(row['unit']),
             'image': imgpat % (int(row['unit']) - 1),
             'phrase': row['label']
-        }
-        print json.dumps(result)
+        })
 
+rng.shuffle(cases)
+for index in range(0, len(cases), batch_size):
+    batch = cases[index:index+batch_size]
+    print json.dumps({ 'cases': batch })
